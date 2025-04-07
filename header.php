448e7b3dd5133560913fac48602e0c13aa9fcfef
nukesaku <nextniconico@gmail.com>
2025-04-07 23:01:15 +0900

    lecture 23 get_template_directory_uri

--- header.php
+++ header.php
@@ -1,14 +1,12 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<meta name="description" content="" />
-<meta name="author" content="" />
-<title>Clean Blog - Start Bootstrap Theme</title>
-<link rel="icon" type="image/x-icon" href="/wp-06/wp-content/themes/theme_of_udemy/assets/favicon.ico" />
+<title><?php the_title() ?></title>
+<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri() ?>/assets/favicon.ico" />
 <!-- Font Awesome icons (free version)-->
-<script src="http://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+<script src="//use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 <!-- Google fonts-->
-<link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
+<link href="//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
 <!-- Core theme CSS (includes Bootstrap)-->
-<link href="/wp-06/wp-content/themes/theme_of_udemy/css/styles.css" rel="stylesheet" />
+<link href="<?php echo get_template_directory_uri() ?>/css/styles.css" rel="stylesheet" />
 <?php wp_head() ?>
