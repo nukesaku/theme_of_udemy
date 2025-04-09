4ab78dcf8c0a2c71b2cff62519646a8967514e92
nukesaku <nextniconico@gmail.com>
2025-04-09 23:22:26 +0900

    lecture 27 wp_get_attachment_image_src / has_post_thumbnail

--- single.php
+++ single.php
@@ -11,8 +11,14 @@
     <?php while (have_posts()): the_post(); ?>
       <!-- Page Header-->
       <?php
-        $id = get_post_thumbnail_id();
-        $img = wp_get_attachment_image_src($id);
+        if (has_post_thumbnail()) {
+          // アイキャッチ画像が設定されている場合
+          $id = get_post_thumbnail_id();
+          $img = wp_get_attachment_image_src($id);
+        } else {
+          // アイキャッチ画像が設定されていない場合
+          $img = array(get_template_directory_uri() . '/assets/img/post-bg.jpg');
+        }
       ?>
       <header class="masthead" style="background-image: url('<?php echo $img[0] ?>')">
         <div class="container position-relative px-4 px-lg-5">
