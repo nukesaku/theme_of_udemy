588c7ecd2c23b8f1256478e3908e562ef6bf3665
nukesaku <nextniconico@gmail.com>
2025-04-09 23:51:57 +0900

    lecture 30 page.php 固定ページのテンプレート作成

--- page.php
+++ page.php
@@ -11,33 +11,29 @@
     <?php while (have_posts()): the_post(); ?>
       <!-- Page Header-->
       <?php
-        if (has_post_thumbnail()) {
-          // アイキャッチ画像が設定されている場合
-          $id = get_post_thumbnail_id();
-          $img = wp_get_attachment_image_src($id, 'large');
-        } else {
-          // アイキャッチ画像が設定されていない場合
-          $img = array(get_template_directory_uri() . '/assets/img/post-bg.jpg');
-        }
+      if (has_post_thumbnail()) {
+        // アイキャッチ画像が設定されている場合
+        $id = get_post_thumbnail_id();
+        $img = wp_get_attachment_image_src($id, 'large');
+      } else {
+        // アイキャッチ画像が設定されていない場合
+        $img = array(get_template_directory_uri() . '/assets/img/about-bg.jpg');
+      }
       ?>
+      <!-- Page Header-->
       <header class="masthead" style="background-image: url('<?php echo $img[0] ?>')">
         <div class="container position-relative px-4 px-lg-5">
           <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
-              <div class="post-heading">
+              <div class="page-heading">
                 <h1><?php the_title(); ?></h1>
-                <span class="meta">
-                  Posted by
-                  <?php the_author() ?>
-                  on <?php the_date() ?>
-                </span>
               </div>
             </div>
           </div>
         </div>
       </header>
-      <!-- Post Content-->
-      <article class="mb-4">
+      <!-- Main Content-->
+      <main class="mb-4">
         <div class="container px-4 px-lg-5">
           <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
@@ -45,9 +41,11 @@
             </div>
           </div>
         </div>
-      </article>
+      </main>
+
     <?php endwhile; ?>
   <?php endif ?>
+
   <?php get_template_part('includes/footer') ?>
   <?php get_footer() ?>
 </body>
