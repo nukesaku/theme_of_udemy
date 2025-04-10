28b3c8a27ce43c28ada06e782842bc9692bcf677
nukesaku <nextniconico@gmail.com>
2025-04-10 20:56:15 +0900

    lecture 32 ファンクション（関数）を作ろう

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
 add_action('init', function () {
-    add_theme_support('post-thumbnails');
+  add_theme_support('post-thumbnails');
 });
+function get_eyecatch_with_default() {
+  if (has_post_thumbnail()) {
+    // アイキャッチ画像が設定されている場合
+    $id = get_post_thumbnail_id();
+    $img = wp_get_attachment_image_src($id, 'large');
+  } else {
+    // アイキャッチ画像が設定されていない場合
+    $img = array(get_template_directory_uri() . '/assets/img/about-bg.jpg');
+  }
+
+  return $img;
+}
--- page.php
+++ page.php
@@ -11,14 +11,7 @@
     <?php while (have_posts()): the_post(); ?>
       <!-- Page Header-->
       <?php
-      if (has_post_thumbnail()) {
-        // アイキャッチ画像が設定されている場合
-        $id = get_post_thumbnail_id();
-        $img = wp_get_attachment_image_src($id, 'large');
-      } else {
-        // アイキャッチ画像が設定されていない場合
-        $img = array(get_template_directory_uri() . '/assets/img/about-bg.jpg');
-      }
+        $img = get_eyecatch_with_default();
       ?>
       <!-- Page Header-->
       <header class="masthead" style="background-image: url('<?php echo $img[0] ?>')">
