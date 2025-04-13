fca9e3202afc4b30292b55fa1f24fec886e5cd94
nukesaku <nextniconico@gmail.com>
2025-04-13 19:34:36 +0900

    lecture 41 メニュー / get_nav_menu_locations / wp_get_nav_menu_object / wp_get_nav_menu_items/ ショートエコー構文

--- includes/header.php
+++ includes/header.php
@@ -3,21 +3,25 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
   <div class="container px-4 px-lg-5">
-    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+    <a class="navbar-brand" href="<?= home_url() ?>">Start Bootstrap</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       Menu
       <i class="fas fa-bars"></i>
     </button>
-    <?php wp_nav_menu([
-      'theme_location' => 'global_nav'
-    ]); ?>
+    <?php
+      // メニューIDを取得
+      $menu_name = 'global_nav';
+      $locations = get_nav_menu_locations();
+      $menu = wp_get_nav_menu_object($locations[$menu_name]);
+
+      // メニュー取得。メニューIDを元に。
+      $menu_items = wp_get_nav_menu_items($menu->term_id);
+    ?>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ms-auto py-4 py-lg-0">
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">ホーム</a></li>
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
-      </ul>
+        <?php foreach ($menu_items as $item): ?>
+          <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="<?= $item->url; ?>"><?= $item->title; ?></a></li>
+        <?php endforeach; ?>
     </div>
   </div>
 </nav>
