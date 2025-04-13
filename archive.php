2b2fd1f23f2912e12634cb2d58c1ea5129ad796e
nukesaku <nextniconico@gmail.com>
2025-04-13 16:19:09 +0900

    lecture 37 タグページをarchive.phpで作成 / is_author / is_date

--- archive.php
+++ archive.php
@@ -14,7 +14,15 @@
       <div class="row gx-4 gx-lg-5 justify-content-center">
         <div class="col-md-10 col-lg-8 col-xl-7">
           <div class="site-heading">
-            <h1>Category</h1>
+            <?php if(is_category()): ?>
+              <h1>Category</h1>
+            <?php elseif(is_author()): ?>
+              <h1>Author</h1>
+            <?php elseif(is_date()): ?>
+              <h1>Date</h1>
+            <?php else: ?>
+              <h1>Tag</h1>
+            <?php endif ?>
             <span class="subheading"><?php wp_title('') ?></span>
           </div>
         </div>
