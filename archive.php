5b4454c65ad2ce8e8f1b66bd68877dcbdb2926e4
nukesaku <nextniconico@gmail.com>
2025-04-13 16:05:33 +0900

    lecture 36 カテゴリーページ作成(archive.php)

--- archive.php
+++ archive.php
@@ -14,8 +14,8 @@
       <div class="row gx-4 gx-lg-5 justify-content-center">
         <div class="col-md-10 col-lg-8 col-xl-7">
           <div class="site-heading">
-            <h1>Clean Blog <?php echo date('Y') ?></h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1>Category</h1>
+            <span class="subheading"><?php wp_title('') ?></span>
           </div>
         </div>
       </div>
